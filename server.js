25ce2ba90a7fd255348efb24ca8fdfce96a6bda9
Emran Hossain <emranhossain0335@gmail.com>
2023-10-11 19:26:56 +0600

    goals are added in the UI

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 5000;
-const connectDB = require("./config/db");
+const connectDB = require("./backend/config/db");
 const colors = require("colors");
-const { errorHandler } = require("./middleware/errorMiddleware");
+const { errorHandler } = require("./backend/middleware/errorMiddleware");
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
@@ -24,21 +24,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 // getting data
-app.use("/api/goals", require("./routes/goalRoutes"));
-app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/goals", require("./backend/routes/goalRoutes"));
+app.use("/api/users", require("./backend/routes/userRoutes"));
 
-// Serve frontend
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+// // Serve frontend
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
-    )
-  );
-} else {
-  app.get("/", (req, res) => res.send("Please set to production"));
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(
+//       path.resolve(__dirname, "../", "frontend", "dist", "index.html")
+//     )
+//   );
+// } else {
+//   app.get("/", (req, res) => res.send("Please set to production"));
+// }
 app.use(errorHandler);
 app.listen(PORT, () =>
   console.log(`server is running on http://localhost:${PORT}`)
