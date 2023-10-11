1ec9a8b45097c989c1af908960d7d8af0ed82cad
Emran Hossain <emranhossain0335@gmail.com>
2023-10-11 08:13:34 +0600

    goals are added in the UI

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const path = require('path')
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv").config();
@@ -6,7 +6,6 @@ const PORT = process.env.PORT || 5000;
 const connectDB = require("./config/db");
 const colors = require("colors");
 const { errorHandler } = require("./middleware/errorMiddleware");
-
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
@@ -16,31 +15,30 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
-
 connectDB();
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors())
+app.use(cors());
 
 // getting data
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
 // Serve frontend
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
-    app.get('*', (req, res)=> {
-        res.sendFile(
-            path.resolve(__dirname, "../", 'frontend', 'build', 'index.html')
-        )
-    }) else {
-        app.get('/', (req, res)=> {
-            res.send('Please set to production')
-        })
-    }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    );
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Please set to production");
+  });
 }
 app.use(errorHandler);
 app.listen(PORT, () =>
