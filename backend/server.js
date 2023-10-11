3a5b2341fe7f55da43aebb440196be9bdd9ecb10
Emran Hossain <emranhossain0335@gmail.com>
2023-10-11 19:06:30 +0600

    goals are added in the UI

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
 // Serve frontend
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "development") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
   app.get("*", (req, res) => {
     res.sendFile(
