f3692a090e93e14f03e26ddf010f0f5e21e4f2ec
Emran Hossain <emranhossain0335@gmail.com>
2023-10-11 07:56:29 +0600

    goals are added in the UI

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv").config();
@@ -29,7 +30,18 @@ app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
 // Serve frontend
-
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.get('*', (req, res)=> {
+        res.sendFile(
+            path.resolve(__dirname, "../", 'frontend', 'build', 'index.html')
+        )
+    }) else {
+        app.get('/', (req, res)=> {
+            res.send('Please set to production')
+        })
+    }
+}
 app.use(errorHandler);
 app.listen(PORT, () =>
   console.log(`server is running on http://localhost:${PORT}`)
